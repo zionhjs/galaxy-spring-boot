86dae165079fe7667e08cbca50b793ed76b59fee
Administrator <1061076142@qq.com>
2021-09-11 10:53:52 +0800

    测试部署2

--- src/main/java/com/galaxy/project/Application.java
+++ src/main/java/com/galaxy/project/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+//测试部署2
 @SpringBootApplication
 @EnableScheduling     //开启定时器
 @ServletComponentScan //该注解可以自动注入@WebServlet，@WebFilter，@WebListener。否则在过滤器中无法获取到自动注入得bean
