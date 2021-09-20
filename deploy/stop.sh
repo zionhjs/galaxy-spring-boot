7ce675f03b48c4e5fc97798eb2107afbdad7cf56
Administrator <1061076142@qq.com>
2021-09-20 18:37:02 +0800

    解决缓存错误问题

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#Service_name=galaxy-spring-boot
-#Docker_running=`sudo docker ps |sudo grep $Service_name|wc -l`
+Service_name=galaxy-spring-boot
+Docker_running=`sudo docker ps |sudo grep $Service_name|wc -l`
 
-#if [ "$Docker_running" -gt 0 ];then
-#     sudo docker rm -f $Service_name >/dev/null 2>1
-#     sudo docker rm -f `sudo docker ps -qa` >/dev/null 2>1
-#     sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
-#else
+if [ "$Docker_running" -gt 0 ];then
+     sudo docker rm -f $Service_name >/dev/null 2>1
+     sudo docker rm -f `sudo docker ps -qa` >/dev/null 2>1
+     sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
+else
      sudo echo 'pass stop.'
-#fi
+fi
 
 
