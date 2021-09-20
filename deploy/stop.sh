fc1593155ef6af80cd0554cdf7510b996943a2c4
Administrator <1061076142@qq.com>
2021-09-20 17:53:13 +0800

    优化脚本

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -3,12 +3,12 @@
 Service_name=galaxy-spring-boot
 Docker_running=`sudo docker ps |sudo grep $Service_name|wc -l`
 
-if [ -n "$Docker_running" ];then
+if [ "$Docker_running" -gt 0 ];then
      sudo docker rm -f $Service_name >/dev/null 2>1
      sudo docker rm -f `sudo docker ps -qa` >/dev/null 2>1
      sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
 else
-     sudo echo 'exit 0'
+     sudo echo 'pass stop.'
 fi
 
 
